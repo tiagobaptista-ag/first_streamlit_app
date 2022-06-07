a6c6c62e9dc720eddc831d91917787ee106d6ce2
tiagobaptista-ag <100767900+tiagobaptista-ag@users.noreply.github.com>
2022-06-07 14:23:36 +0100

    Update streamlit_app.py
    too many cooks in the kitchen

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,8 @@ import streamlit
 streamlit.title('Too many cooks')
 
 streamlit.header('Too many cooks')
-
+streamlit.text('Too many cooks')
+streamlit.text('Too many cooks')
+streamlit.text('Too many cooks')
+streamlit.text('Too many cooks')
                 
