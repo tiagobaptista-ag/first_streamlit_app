66328d4aa967a8d31637bf85fb1259ae0f0c6bb6
tiagobaptista-ag <100767900+tiagobaptista-ag@users.noreply.github.com>
2022-06-07 14:26:55 +0100

    Update streamlit_app.py
    cmon...

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,3 +14,5 @@ streamlit.text('A scoop of kids to add the spice')
 streamlit.text('A dash of love to make it nice')
 streamlit.text('And you've got...')
 streamlit.text('🥣 🥗 🐔 🥑🍞')
+
+               
